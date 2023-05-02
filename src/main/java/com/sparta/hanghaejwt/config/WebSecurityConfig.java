57f033a4f8c276af0385e23f4bae17ceb1e207c7
kes <kmskes1125@gmail.com>
2023-05-02 15:24:08 +0900

    WebSecurityConfig.java 커밋
    Spring Security를 사용하기 위해 초기 세팅을 함

---
+++ src/main/java/com/sparta/hanghaejwt/config/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.sparta.hanghaejwt.config;
+
+
+import com.sparta.hanghaejwt.jwt.JwtAuthFilter;
+import com.sparta.hanghaejwt.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
+public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+                .antMatchers("/api/user/**").permitAll()
+                //antMatchers - url 지정, permitAll - 앞 url 허가 -> security에 걸리지 않고 넘어가게 함
+                // method 관련 에러 - .antMatchers(HttpMethod.GET, "/api/posts/**").permitAll() 이런 식으로 해결
+                //권한 체크, 인증 등등 토큰 검사
+                .anyRequest().authenticated()
+                // JWT 인증/인가를 사용하기 위한 설정
+                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
