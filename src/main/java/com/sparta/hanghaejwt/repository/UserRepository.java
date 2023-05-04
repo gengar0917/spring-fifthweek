21510604eefadc263e69a100ecbc87a05c7a3cdf
kes <kmskes1125@gmail.com>
2023-05-04 20:09:46 +0900

    @Repository 어노테이션 추가

--- src/main/java/com/sparta/hanghaejwt/repository/UserRepository.java
+++ src/main/java/com/sparta/hanghaejwt/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.sparta.hanghaejwt.repository;
 
 import com.sparta.hanghaejwt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
